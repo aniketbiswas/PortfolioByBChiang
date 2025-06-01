b312dfe8bbaed6bf38b37c0cb2bc2045957a8e42
Aniket Biswas <abiswas@microsoft.com>
2025-06-01 13:04:48 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7ae9113d5e9f4eb13614.js"
+    "url": "webpack-runtime-ebec25b771c6efe1d368.js"
   },
   {
     "url": "framework-d640695159b288304a04.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4815e617be047a0a8564fec1d84046ee"
+    "revision": "79d0a2f0da83b06ef335e8a81f809d3c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-69e2a473a13da6d6a3e2.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "1639b83171854b700d3a0acffba00c84"
+    "revision": "b19603a48178cf9d10fa28bb8823c1e6"
   },
   {
     "url": "manifest.webmanifest",
